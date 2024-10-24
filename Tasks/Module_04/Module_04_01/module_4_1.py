4f8de7624c476d3f366dd983ec9a841a5130ab86
AlexeyCheremisin <annacherrymissy@gmail.com>
2024-10-24 08:10:27 -0400

    Create module_4_1.py

---
+++ Tasks/Module_04/Module_04_01/module_4_1.py
@@ -0,0 +1,13 @@
+from fake_math import divide as fake_divide
+from true__math import divide as true_divide
+
+if __name__ == "__main__":
+    result_1 = fake_divide(69, 3)
+    result_2 = fake_divide(3, 0)
+    result_3 = true_divide(49, 7)
+    result_4 = true_divide(15, 0)
+    print(result_1)
+    print(result_2)
+    print(result_3)
+    print(result_4)
+    
